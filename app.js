03086707acafbbb27c5e38918ce98e7281536e7e
jasemarq <jasemarq.dev@gmail.com>
2017-06-19 19:10:30 -0400

    TDD

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const config = require('./config/database');
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+const http = require("http");
 
 // Connect to database
 mongoose.connect(config.database);
 
 // On Connection
-mongoose.connection.on('connected', () => 
+mongoose.connection.on("connected", () => 
 {
-	console.log('Connected to database '+ config.database);
+	console.log("Connected to database "+ config.database);
 });
 
-mongoose.connection.on('error', (err) => 
+mongoose.connection.on("error", (err) => 
 {
- console.log('Databse error: '+err);
-})
+	console.log("Databse error: "+err);
+});
 
 const app = express();
 
 // API route inclusion
-const users = require('./routes/users');
+const users = require("./routes/users");
 
 
 // Port Number
-app.set('port', process.env.PORT || 8000);
+app.set("port", process.env.PORT || 8000);
 
 
 // CORS Middleware
 app.use(cors());
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 
 // Users Routes
-app.use('/users', users);
+app.use("/users", users);
 
 // Index Route
-app.get('/', (req, res) => 
+app.get("/", (req, res) => 
 {
-	res.send('Invalid endpoint');
-})
+	res.send("Invalid endpoint");
+});
 
 // Start Server
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function(){
+	console.log("Express server listening on port " + app.get("port"));
 });
---
+++ test/test-server.js
@@ -0,0 +1,7 @@
+describe('User', function() {
+	it('should list ALL users on /users GET');
+	it('should list a SINGLE user on /users/<id> GET');
+	it('should add a SINGLE user on /users POST');
+	it('should update a SINGLE user on /users/<id> POST');
+	it('should delete a SINGLE user on /user/<id> DELETE');
+})
